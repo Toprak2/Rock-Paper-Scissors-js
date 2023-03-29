47a3eb602d381fcd5564185c4e1b62898beaf17d
Muhammed Ali <muhammedalitoprak1@gmail.com>
2023-03-30 00:59:59 +0300

    playRound() now shows result in DOM rather that console

--- script.js
+++ script.js
@@ -11,65 +11,69 @@ function getComputerChoice(){
 }
 let playerScore=0;
 
+
 function playRound(playerChoice){
+    //create referencer to gameAnnouncer and scoreboard divs
+    const gameAnnouncer = document.querySelector(".gameAnnouncer");
+    const scoreboard = document.querySelector(".scoreboard");
 
     //Check possible win conditions for when playerInput is paper, if player won increase the playerScore
     if(playerChoice=="paper"){
         if(getComputerChoice() == "Rock"){
             playerScore++;
-            console.log( "Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+"You win! Paper Beats Rock");
+            gameAnnouncer.textContent= "Your Choice : Paper\nComputers Choice: "+getComputerChoice()+"You win! Paper Beats Rock";
             
         }
         else if(getComputerChoice=="Scissors"){
-            console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+"You Lose! Scissors Beats Paper");
+            gameAnnouncer.textContent="Your Choice : Paper\nComputers Choice: "+getComputerChoice()+"You Lose! Scissors Beats Paper";
         }
         else{
-        console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+" Tie!");
+        gameAnnouncer.textContent = "Your Choice : Paper\nComputers Choice: "+getComputerChoice()+" Tie!";
         }
     } 
     //Check possible win conditions for when playerInput is scissors, if player won increase the playerScore
     if(playerChoice=="scissors"){
         if(getComputerChoice() == "Rock"){
-            console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+"You Lose! Rock Beats Scissors");
+            gameAnnouncer.textContent = "Your Choice : Scissors \nComputers Choice: "+getComputerChoice()+"You Lose! Rock Beats Scissors";
         }
         else if(getComputerChoice()=="Paper"){
             playerScore++;
-            console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+"You Win! Scissors Beats Paper");
+            gameAnnouncer.textContent = "Your Choice : Scissors\nComputers Choice: "+getComputerChoice()+"You Win! Scissors Beats Paper";
             
         }else {
-        console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+" Tie!");       
+        gameAnnouncer.textContent = "Your Choice : Scissors\nComputers Choice: "+getComputerChoice()+" Tie!";      
         }
     }   
     //Check possible win conditions for when playerInput is rock, if player won increase the playerScore
     if(playerChoice=="rock"){
         if(getComputerChoice() == "Scissors"){
             playerScore++;
-            console.log( "Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+"You Win! Rock Beats Scissors");
+            gameAnnouncer.textContent =  "Your Choice : Rock\nComputers Choice: "+getComputerChoice()+"You Win! Rock Beats Scissors";
             
         }
         else if(getComputerChoice()=="Paper"){
-            console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+"\nYou Lose! Paper Beats Rock");
+            gameAnnouncer.textContent = "Your Choice : Rock\nComputers Choice: "+getComputerChoice()+"\nYou Lose! Paper Beats Rock";
         }
         else{
-        console.log("Your Choice :"+playerChoice+"\nComputers Choice:"+getComputerChoice()+" Tie!");   
+        gameAnnouncer.textContent = "Your Choice : Rock\nComputers Choice: "+getComputerChoice()+" Tie!";   
         }  
     }
 }
-//Declare the rock button
+//Declare a reference to the rock button
 const buttonRock=document.querySelector(".rock");
-//add a function to trigger on click to play the round with the buttons class value;
+//add a function to trigger on click to play the round with the buttons class value as playerChoice;
 buttonRock.addEventListener("click",function(){
     playRound(this.classList.value); 
 })
-
+//Declare a reference to the paper button
 const buttonPaper=document.querySelector(".paper");
-
+//add a function to trigger on click to play the round with the buttons class value as playerChoice;
 buttonPaper.addEventListener("click",function(){
     playRound(this.classList.value); 
 })
-
+//Declare a reference to the scissors button
 const buttonScissors=document.querySelector(".scissors");
-
+//add a function to trigger on click to play the round with the buttons class value as playerChoice;
 buttonScissors.addEventListener("click",function(){
    playRound(this.classList.value); 
 })
